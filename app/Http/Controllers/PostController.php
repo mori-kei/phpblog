277d9ee568eb5396b9e4540f40640ad0bf6df829
mori-kei <mojavascript13@gmail.com>
2022-12-02 16:14:18 +0900

    ブログ投稿一覧表示機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\Post;
 
@@ -9,7 +8,8 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return $post->get();
+         return view('posts/index')->with(['posts' => $post->get()]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
     

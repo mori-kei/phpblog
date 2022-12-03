cbb33683d353ea50136af308f3548940ae96c105
mori-kei <mojavascript13@gmail.com>
2022-12-03 18:21:10 +0900

    新規投稿機能バリーデーションの追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
-
+use App\Http\Requests\PostRequest;
 class PostController extends Controller
 {
     public function index(Post $post)
@@ -19,5 +19,13 @@ class PostController extends Controller
     public function create()
     {
         return view("posts/create");
+    }
+    
+    public function store(PostRequest $request,Post $post)
+    {
+    
+        $input = $request["post"];
+        $post->fill($input)->save();
+        return redirect("posts/" .$post->id);
     }
 }

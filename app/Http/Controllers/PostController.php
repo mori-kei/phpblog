6cfd30b40f3456e266a3f6a76f1cf5ffb8f4cbd4
mori-kei <mojavascript13@gmail.com>
2022-12-07 17:34:48 +0900

    カテゴリ機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\PostRequest;
+use App\Models\Category;
 class PostController extends Controller
 {
     public function index(Post $post)
@@ -16,9 +17,9 @@ class PostController extends Controller
             return view("posts/show")-> with(["post" =>$post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view("posts/create");
+        return view('posts/create')->with(['categories' => $category->get()]);
     }
     
     public function store(PostRequest $request,Post $post)

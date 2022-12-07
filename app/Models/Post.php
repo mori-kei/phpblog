dad914ada35bf6b807e2bd345ad62a3be4bfbc04
mori-kei <mojavascript13@gmail.com>
2022-12-07 14:13:01 +0900

    削除機能の追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use HasFactory;
-    
+    use SoftDeletes;
     public function getPaginateByLimit(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
